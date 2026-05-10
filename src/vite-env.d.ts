22fb27636168aa7d03fb297623bbfc08279cd9a4
DanyHudson <mail.d.hudson@gmail.com>
2026-05-10 20:45:06 +0200

    file structure

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="vite/client" />
 declare module '*.scss';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.webp';
+declare module '*.svg';
