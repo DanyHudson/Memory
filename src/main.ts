73d17063c17b90fa632281059a86ddee2e82acbd
DanyHudson <mail.d.hudson@gmail.com>
2026-05-19 23:08:25 +0200

    started  settings logic

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { BOARD_SIZES } from './data/board-sizes';
 import { INITIAL_GAME_STATE } from './state/initial-state';
 
 let gameState: GameState = structuredClone(INITIAL_GAME_STATE);
+let playerId: PlayerId = 'blue';
+let boardSizeId: number = 16;
+let themeId: string = 'da-projects';
+
 
 // gameState = gameState;
 
@@ -49,40 +53,15 @@ function renderSettings(rootElement: HTMLElement) {
             <h1>Settings</h1>
         </header>
 
+        <div class="settings-wrapper">
+
         <div class="settings-select">
 
-           <!-- <div class="settings-item">
-                <div class="settings-icon-container">
-                    <img src="" alt="Colour Palette Icon" class="settings-icon">
-                </div>
-                <div class="settings-options-container">
-                    <h2 class="settings-title">Game Themes</h2>
-                    <div class="settings-options">
-                        <input type="radio" id="da-projects-theme" name="game-theme"> <label for="da-projects-theme">DA Projects Theme</label>
-                        <input type="radio" id="foods-theme" name="game-theme"> <label for="foods-theme">Foods Theme</label>
-                    </div>
-                </div>
-            </div> -->
-
-              <fieldset class="settings-item">
-              <legend class="settings-title"><img src="" alt="" class="settings-icon">Game Themes</legend>
+            <fieldset class="settings-item">
+            <legend class="settings-title"><img src="" alt="" class="settings-icon">Game Themes</legend>
                     <div class="settings-option"><input type="radio" id="da-projects-theme" name="game-theme"> <label for="da-projects-theme">DA Projects Theme</label></div>
                     <div class="settings-option"><input type="radio" id="foods-theme" name="game-theme"> <label for="foods-theme">Foods Theme</label></div>
-              </fieldset> 
-
-
-            <!--<div class="settings-item">
-                <div class="settings-icon-container">
-                    <img src="" alt="Player Icon" class="settings-icon">
-                </div>
-                <div class="settings-options-container">
-                    <h2 class="settings-title">Player</h2>
-                    <div class="settings-options">
-                        <input type="radio" id="blue-player" name="player"> <label for="blue-player">Blue Player</label>
-                        <input type="radio" id="orange-player" name="player"> <label for="orange-player">Orange Player</label>
-                    </div>
-                </div>
-            </div> -->
+            </fieldset> 
 
             <fieldset class="settings-item">
             <legend class="settings-title"><img src="" alt="" class="settings-icon">Player</legend> 
@@ -90,23 +69,8 @@ function renderSettings(rootElement: HTMLElement) {
                     <div class="settings-option"><input type="radio" id="orange-player" name="player"> <label for="orange-player">Orange Player</label></div>
             </fieldset>
 
-
-           <!-- <fieldset class="settings-item">
-                <div class="settings-icon-container">
-                    <img src="" alt="Mobile Icon" class="settings-icon">
-                </div>
-                <div class="settings-options-container">
-                    <legend class="settings-title">Board Size</legend>
-                    <div class="settings-options">
-                        <input type="radio" id="16-cards" name="board-size"> <label for="16-cards">16 Cards</label>
-                        <input type="radio" id="24-cards" name="board-size"> <label for="24-cards">24 Cards</label>
-                        <input type="radio" id="36-cards" name="board-size"> <label for="36-cards">36 Cards</label>
-                    </div>
-                </div>
-            </fieldset> -->
-
             <fieldset class="settings-item">
-            <legend class="settings-title"><img src="" alt="" class="settings-icon" style="width: 24px; height: 24px; background-color: rgb(255, 255, 255);">Board Size</legend>          
+            <legend class="settings-title"><img src="" alt="" class="settings-icon">Board Size</legend>          
                     <div class="settings-option"><input type="radio" id="16-cards" name="board-size"> <label for="16-cards">16 Cards</label></div>
                     <div class="settings-option"><input type="radio" id="24-cards" name="board-size"> <label for="24-cards">24 Cards</label></div>
                     <div class="settings-option"><input type="radio" id="36-cards" name="board-size"> <label for="36-cards">36 Cards</label></div>
@@ -115,13 +79,19 @@ function renderSettings(rootElement: HTMLElement) {
         </div>
 
         <div class="settings-visual">
-            <div class="visual-shot"><img src="" alt="">Screen Shot</div>
-            <div class="settings-confirmed"> Game Theme / Player / Board Size </div>
+            <div class="visual-shot"><img src="" alt=""></div>
+            <div class="settings-confirmed"> ${themeId} / ${playerId} / ${boardSizeId} </div>
         </div>
 
+    </div>
+
     </section>
+
+
     </main>
     `;
+
+    addSettingsListeners(rootElement);
 }
 
 function startGame(rootElement: HTMLElement) {
@@ -140,6 +110,24 @@ function addSettingsListeners(rootElement: HTMLElement) {
     const themeOptions = rootElement.querySelectorAll<HTMLInputElement>('input[name="game-theme"]');
     const playerOptions = rootElement.querySelectorAll<HTMLInputElement>('input[name="player"]');
     const boardSizeOptions = rootElement.querySelectorAll<HTMLInputElement>('input[name="board-size"]');
+
+    themeOptions.forEach((option) => {
+        option.addEventListener('change', () => {
+            themeId = option.id;
+        });
+    });
+
+    playerOptions.forEach((option) => {
+        option.addEventListener('change', () => {
+            playerId = option.id as PlayerId;
+        });
+    });
+    
+    boardSizeOptions.forEach((option) => {
+        option.addEventListener('change', () => {
+            boardSizeId = parseInt(option.id.split('-')[0]);
+        });
+    });
 } 
 
 render(root);   
